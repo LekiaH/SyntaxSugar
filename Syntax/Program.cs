5a4b2aa898b1d70fcf01a290b195905bc5f0ec6f
LekiaH <lekia.hood@gmail.com>
2022-05-04 17:29:20 -0400

    initial commit

---
+++ Syntax/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Syntax
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int answer = 4;
+            var response = (answer < 9);
+
+            if (answer < 9)
+            {
+                Console.WriteLine($"{answer} is less than 9.");
+            }
+            else
+            {
+                Console.WriteLine($"{answer} is greater than 9.");
+            }
+
+
+        }
+    }
+}
